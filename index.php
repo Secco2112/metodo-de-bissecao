dcb7d857b9d465c135f667791c7ec141bd807faf
Gustavo Marmentini <gustavomarini9@gmail.com>
2018-08-11 18:22:13 -0300

    Remove theme and update index

--- index.php
+++ index.php
@@ -82,6 +82,5 @@
 	<script src="includes/js/jquery.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-	<script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
 	<script src="includes/js/script.js" type="text/javascript"></script>
 </html>
